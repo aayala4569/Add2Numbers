87e9aed3305bd2abcafb627b83bf98206e6674c0
aayala4569 <angelicaayala100@gmail.com>
2022-10-20 22:03:38 -0700

    Add2Numbers

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Angelica Ayala
+//Date Revised: 8/9/22
+//Mini Challenge #2- "Adding 2 Numbers
+//---------Description--------
+//Display a question asking user for a number1
+//Initialize a variable and convert input to a interger
+//Displayed another question asking user for number2
+//Iinitialize another variable and convert input to interger
+//Result will be the total Interprelated
+
+
+Console.WriteLine("Enter a number");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter a second number");
+
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"your answer is{number1 + number2}");
